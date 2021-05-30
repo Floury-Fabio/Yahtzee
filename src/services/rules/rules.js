30714b2833f0a428b237535836aec591863eace6
floury-fabio <floury-fabio@hotmail.fr>
2021-05-30 13:21:07 +0200

    fixup! add Lower rule

--- src/services/rules/rules.js
+++ src/services/rules/rules.js
@@ -4,6 +4,13 @@ import ThreesRule from 'services/rules/ThreesRule';
 import FoursRule from 'services/rules/FoursRule';
 import FivesRule from 'services/rules/FivesRule';
 import SixesRule from 'services/rules/SixesRule';
+import ChanceRule from 'services/rules/ChanceRule';
+import FourOfKindRule from 'services/rules/FourOfKindRule';
+import FullHouseRule from 'services/rules/FullHouseRule';
+import LargeStraightRule from 'services/rules/LargeStraightRule';
+import SmallStraightRule from 'services/rules/SmallStraightRule';
+import ThreeOfKindRule from 'services/rules/ThreeOfKindRule';
+import Yahtzee from 'services/rules/Yahtzee';
 
 const upperRules = [
   new OnesRule(),
@@ -15,7 +22,13 @@ const upperRules = [
 ];
 
 const lowerRules = [
-  new OnesRule(),
+  new ChanceRule(),
+  new FourOfKindRule(),
+  new FullHouseRule(),
+  new LargeStraightRule(),
+  new SmallStraightRule(),
+  new ThreeOfKindRule(),
+  new Yahtzee(),
 ];
 
 export { lowerRules, upperRules };
