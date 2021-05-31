2d624b49ee4856e23d4b011030d76e27c2a254e1
floury-fabio <floury-fabio@hotmail.fr>
2021-05-31 13:11:17 +0200

    add reset rollscount when a rule is selected

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,31 +1,14 @@
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-
-import RollButton from 'components/RollButton';
 
 import DiceSet from 'components/DiceSet';
 
-const GameBoard = ({ diceList, setDiceList }) => {
-  const [rollsCount, setRollsCount] = useState(2);
+const GameBoard = ({ diceList, setDiceList }) => (
+  <div>
+    <DiceSet diceList={diceList} setDiceList={setDiceList} />
+  </div>
+);
 
-  const rollDices = () => {
-    const list = diceList.map((dice) => {
-      const randValue = Math.floor(Math.random() * 6) + 1;
-      if (dice.locked) { return { ...dice, validated: true }; }
-      return { ...dice, value: randValue };
-    });
-    setRollsCount((currentRollsCount) => currentRollsCount - 1);
-    setDiceList(list);
-  };
-
-  return (
-    <div>
-      <h2> Yahtzee!! </h2>
-      <DiceSet diceList={diceList} setDiceList={setDiceList} />
-      <RollButton rollDices={rollDices} rollsCount={rollsCount} />
-    </div>
-  );
-};
 export default GameBoard;
 
 GameBoard.propTypes = {
--- src/components/Yahtzee.jsx
+++ src/components/Yahtzee.jsx
@@ -2,11 +2,14 @@ import React, { useState } from 'react';
 
 import GameBoard from 'components/GameBoard';
 import RulesList from 'components/RulesList';
+import RollButton from 'components/RollButton';
 
 import { upperRules, lowerRules } from 'services/rules/rules';
 
 const Yahtzee = () => {
+  const [rollsCount, setRollsCount] = useState(2);
   const [scores, setScores] = useState({});
+
   const [diceList, setDiceList] = useState(() => {
     const list = new Array(5).fill(1);
     return list.map(() => {
@@ -18,13 +21,26 @@ const Yahtzee = () => {
   const evalScore = (rule) => {
     const dicesValues = diceList.map((dice) => dice.value);
     setScores({ ...scores, [rule.name]: rule.calcScore(dicesValues) });
+    setRollsCount(2);
+  };
+
+  const rollDices = () => {
+    const list = diceList.map((dice) => {
+      const randValue = Math.floor(Math.random() * 6) + 1;
+      if (dice.locked) { return { ...dice, validated: true }; }
+      return { ...dice, value: randValue };
+    });
+    setRollsCount((currentRollsCount) => currentRollsCount - 1);
+    setDiceList(list);
   };
 
   const score = Object.values(scores).reduce((acc, currentValue) => acc + currentValue, 0);
 
   return (
     <div>
-      <GameBoard diceList={diceList} setDiceList={setDiceList} />
+      <h2> Yahtzee!! </h2>
+      <GameBoard diceList={diceList} setDiceList={setDiceList} rollsCount={rollsCount} />
+      <RollButton rollDices={rollDices} rollsCount={rollsCount} />
       <h3> Upper </h3>
       <RulesList rules={upperRules} evalScore={evalScore} scores={scores} />
       <h3> Lower </h3>
